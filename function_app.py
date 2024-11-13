4359a610d8b6803c5d0b05d01e3edeea69424143
fverm <francois.vermelle@bpce-si.fr>
2024-11-13 12:06:04 +0100

    commit #8

--- function_app.py
+++ function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+import pkg_resources
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -17,7 +18,10 @@ def trad01(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        installed_packages = pkg_resources.working_set
+        installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+        for i in installed_packages])
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. Packages: {installed_packages_list}")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
